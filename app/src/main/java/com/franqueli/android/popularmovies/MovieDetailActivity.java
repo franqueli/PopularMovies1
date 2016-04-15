0441d65612c07d23bafc37ce40ec87997f758f9f
Franqueli Mendez <franqueli@gmail.com>
2016-04-14 23:11:38 -0400

    Code clean up

--- app/src/main/java/com/franqueli/android/popularmovies/MovieDetailActivity.java
+++ app/src/main/java/com/franqueli/android/popularmovies/MovieDetailActivity.java
@@ -1,26 +1,10 @@
 package com.franqueli.android.popularmovies;
 
 import android.content.Intent;
-import android.net.Uri;
-import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
-
-import com.franqueli.android.popularmovies.model.MovieInfo;
-import com.orm.SugarRecord;
-import com.squareup.picasso.Picasso;
-
-import java.io.IOException;
-import java.text.NumberFormat;
-import java.util.Calendar;
 
 public class MovieDetailActivity extends AppCompatActivity {
 
@@ -28,9 +12,6 @@ public class MovieDetailActivity extends AppCompatActivity {
     public static final String MOVIE_ID_PARAM = "com.franqueli.android.popularmovies.MOVIE_ID";
 
     private static final String LOG_TAG = MovieDetailActivity.class.getSimpleName();
-
-    private TheMovieDBAPI movieDBAPI;
-    private MovieDetailFragment movieDetailFragment;
 
 
     @Override
@@ -54,7 +35,7 @@ public class MovieDetailActivity extends AppCompatActivity {
             }
 
             // Create a new Fragment to be placed in the activity layout
-            movieDetailFragment = MovieDetailFragment.newInstance(movieIdParam);
+            MovieDetailFragment movieDetailFragment = MovieDetailFragment.newInstance(movieIdParam);
 
             // In case this activity was started with special instructions from an
             // Intent, pass the Intent's extras to the fragment as arguments
